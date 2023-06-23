997934b33461d2fe60d48f4bfbff64a1f6a08684
Suraj Kumar <navneetraj07903@gmail.com>
2023-06-23 16:17:20 +0530

    Project Complete

---
+++ src/main/java/com/satoruakiyama/employeemanagementsystem/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.satoruakiyama.employeemanagementsystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.satoruakiyama.employeemanagementsystem.domain.Employee_Management_System;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee_Management_System, Long> {
+	// This will be AUTO IMPLEMENTED by Spring into a Bean
+}
---
+++ src/test/java/com/satoruakiyama/employeemanagementsystem/EmployManagementSystemApplicationTests.java
@@ -0,0 +1,14 @@
+package com.satoruakiyama.employeemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+class EmployManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
